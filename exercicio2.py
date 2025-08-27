89ed1136f6c6338a352053255f10b8bb3db1f95e
Daniel Ferreira <danielsilvaferreira151006@gmail.com>
2025-08-26 21:55:52 -0300

    Create exercicio2.py

---
+++ exercicio2.py
@@ -0,0 +1,32 @@
+#Exercicío 2
+#Sistema de cadastro de pacientes
+#1-Cadastrar paciente 
+#2-Mostrar paciente cadastrado 
+#3-Atender paciente 
+#4-Sair
+
+def menu():
+    print("\n---Sistema de cadastro de pacientes---")
+    print("1- Registrar Paciente ")
+    print("2- Mostrar paciente cadastrado")
+    print("3- Atender paciente")
+    print("4- listar pacientes registrados")
+    print("0- Sair...")
+    return input("Escolha uma opção")
+     
+    paciente = [None]
+    
+while True:
+    opção = menu ()
+        
+    if opção == "1"
+        paciente = input ("Digite o nome do paciente:")
+        print(f"Paciente '{paciente}'cadastrado ")
+        
+    elif opção == "2":
+        if paciente is None
+        print("nenhum paciente registrado")
+    else:
+       
+   
+    
